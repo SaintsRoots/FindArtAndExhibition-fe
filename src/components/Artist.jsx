aefbb373555eb4b879bd9192b58318d35df06527
Muhoza John <muhozajohn250@gmail.com>
2024-06-03 23:54:10 +0200

    Addinf artist

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,7 +1,9 @@
+import ArtistPage from "../pages/Artist";
+
 const Artist = () => {
   return (
     <div className="container min-h-screen  mx-auto px-6 md:px-14  flex flex-col gap-6 items-start justify-center   ">
-      Artist
+      <ArtistPage />
     </div>
   );
 };
